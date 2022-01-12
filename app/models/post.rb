988f83993580641260255291ee2706f4dd750f73
James Burns <jrburns96@aol.com>
2022-01-12 17:57:29 -0500

    commit tree d02a89c371d169d90a60a3c356f88f4c9a5eb570

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :allow_title
+
+    def allow_title
+        # title
+
+        if title=="True Facts"
+            errors.add(:title, "Title cannot be true facts")
+        end
+    end
 end
